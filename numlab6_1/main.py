a63569813f8f4a8e1781069eb5edbe55e0a81ff2
ggermashev <ggermashev@gmail.com>
2023-02-11 10:58:10 +0300

    errors

--- numlab6_1/main.py
+++ numlab6_1/main.py
@@ -3,16 +3,33 @@ from matplotlib import pyplot as plt
 from matplotlib.animation import ArtistAnimation
 
 h = 0.025
-c = 0.8
+c = 0.7
 start = 0
 end = 10
 initStart = 1
 initEnd = 2
 
-def exp_func(x,x_0,e):
+
+def exp_func(x, x_0, e):
     if abs(x - x_0) >= e:
         return 0
-    return np.exp(-(x-x_0)**2/(e**2 - (x-x_0)**2))
+    return np.exp(-(x - x_0) ** 2 / (e ** 2 - (x - x_0) ** 2))
+
+
+def get_real_sqr(i, x):
+    if (initStart + i * c * h <= x <= initEnd + i * c * h):
+        return 1
+    return 0
+
+
+def get_real_exp(i, x, x_0, e):
+    if abs(x - x_0 - i * c * h) >= e:
+        return 0
+    return np.exp(-(x - x_0 - i * c * h) ** 2 / (e ** 2 - (x - x_0 - i * c * h) ** 2))
+
+
+def get_error(func, realFunc):
+    return max(abs(func[i] - realFunc[i]) for i in range(len(func)))
 
 
 def plot_angle_sqr():
@@ -24,22 +41,26 @@ def plot_angle_sqr():
 
     frames = []
     frames.append(ax.plot(x, u, color='b'))
+    frames.append(ax.plot(x, [get_real_sqr(0, point) for point in x], color='g'))
 
-
+    errors = []
     for k in range(len(x)):
         v = [0 if j == 0 else u[j] - c * (u[j] - u[j - 1]) for j in range(len(x))]
+        realFunc = [get_real_sqr(k + 1, point) for point in x]
         line, = ax.plot(x, v, color='b')
-        frames.append([line])
+        realLine, = ax.plot(x, realFunc, color='g')
+        frames.append([line, realLine])
+        errors.append(get_error(v, realFunc))
         u, v = v, u
         current += 1
 
-
+    print(errors)
     animation = ArtistAnimation(
-        fig,                # фигура, где отображается анимация
-        frames,              # кадры
-        interval=30,        # задержка между кадрами в мс
-        blit=True,          # использовать ли двойную буферизацию
-        repeat=False)       # зацикливать ли анимацию
+        fig,  # фигура, где отображается анимация
+        frames,  # кадры
+        interval=30,  # задержка между кадрами в мс
+        blit=True,  # использовать ли двойную буферизацию
+        repeat=False)  # зацикливать ли анимацию
 
     animation.save('angle_sqr.gif', writer='imagemagick')
 
@@ -54,15 +75,22 @@ def plot_angle_exp():
     fig, ax = plt.subplots()
 
     frames = []
-    frames.append(ax.plot(x, u, color='b'))
+    line, = ax.plot(x, u, color='b')
+    func = [get_real_exp(0, point, x_0, e) for point in x]
+    realLine, = ax.plot(x, func, color='g')
+    frames.append([line, realLine])
 
+    errors = []
     for k in range(len(x)):
         v = [0 if j == 0 else u[j] - c * (u[j] - u[j - 1]) for j in range(len(x))]
+        realFunc = [get_real_exp(k + 1, point, x_0, e) for point in x]
         line, = ax.plot(x, v, color='b')
-        frames.append([line])
+        realLine, = ax.plot(x, realFunc, color='g')
+        frames.append([line, realLine])
+        errors.append(get_error(v, realFunc))
         u, v = v, u
         current += 1
-
+    print(errors)
     animation = ArtistAnimation(
         fig,  # фигура, где отображается анимация
         frames,  # кадры
@@ -91,15 +119,14 @@ def plot_quatro_sqr():
         current += 1
 
     animation = ArtistAnimation(
-        fig,                # фигура, где отображается анимация
-        frames,              # кадры
-        interval=30,        # задержка между кадрами в мс
-        blit=True,          # использовать ли двойную буферизацию
-        repeat=False)       # зацикливать ли анимацию
+        fig,  # фигура, где отображается анимация
+        frames,  # кадры
+        interval=30,  # задержка между кадрами в мс
+        blit=True,  # использовать ли двойную буферизацию
+        repeat=False)  # зацикливать ли анимацию
 
     animation.save('quatro_sqr.gif', writer='imagemagick')
 
 
 plot_angle_sqr()
 plot_angle_exp()
-
